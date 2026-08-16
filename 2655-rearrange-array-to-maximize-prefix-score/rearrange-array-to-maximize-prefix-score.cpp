f199ce111c8681e297bd12ed5baa7179faea737a
Kavish Paraswar <162621258+Kavish-Paraswar@users.noreply.github.com>
2026-08-16 19:20:54 +0530

    Time: 483 ms (6.26%) | Memory: 91.9 MB (29.08%) - LeetSync

--- 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.cpp
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int maxScore(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.end());
-
         long long cur_sum = 0;
         int count = 0;
+        priority_queue<int> pq;
 
         for (int i = 0; i < nums.size(); i++) {
-            cur_sum += nums[i];
-            if (cur_sum > 0)
+
+            if (nums[i] <= 0)
+                pq.push(nums[i]);
+            else {
+                cur_sum += nums[i];
                 count++;
+            }
         }
+        cout << cur_sum << " " << count << endl;
+
+        while (!pq.empty()) {
+            int val = pq.top();
+            cur_sum += val;
+            if (cur_sum <= 0)
+                break;
+            cout << val << " ";
+            count++;
+            pq.pop();
+        }
+
         return count;
     }
 };
