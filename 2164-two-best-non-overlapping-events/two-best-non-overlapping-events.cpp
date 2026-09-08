558e197dfd68b5c9ed69b83bf0298d20f39a38ad
Kavish Paraswar <162621258+Kavish-Paraswar@users.noreply.github.com>
2026-09-08 19:32:13 +0530

    Time: 97 ms (67.94%) | Memory: 192.9 MB (92.51%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -2,46 +2,31 @@ class Solution {
 public:
     static bool comp(vector<int>& a, vector<int>& b) {
         if (a[2] != b[2])
-            return a[2] > b[2];
-
-        return a[0] < b[0];
-    }
-
-    static bool comp2(vector<int>& a, vector<int>& b) {
-        if (a[0] != b[0])
             return a[0] < b[0];
 
         return a[1] < b[1];
     }
-
     int maxTwoEvents(vector<vector<int>>& events) {
         int n = events.size();
 
-        vector<vector<int>> v = events;
-
         sort(events.begin(), events.end(), comp);
-        sort(v.begin(), v.end(), comp2);
-
-        vector<int> maxi(n);
-        maxi[n - 1] = v[n - 1][2];
-
-        for (int i = n - 2; i >= 0; i--)
-            maxi[i] = max(maxi[i + 1], v[i][2]);
-
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            int cur = events[i][2];
-            int cur_end = events[i][1];
+        vector<int> post_sum(n, 0);
+        post_sum[n - 1] = events[n - 1][2];
 
-            int lefty = 0;
-            int righty = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            post_sum[i] = max(post_sum[i + 1], events[i][2]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int cur = events[i][2], lefty = i + 1, righty = n - 1;
             int pos = n;
 
             while (lefty <= righty) {
                 int mid = lefty + (righty - lefty) / 2;
 
-                if (v[mid][0] > cur_end) {
+                if (events[mid][0] > events[i][1]) {
                     pos = mid;
                     righty = mid - 1;
                 } else {
@@ -50,9 +35,8 @@ public:
             }
 
             ans = max(ans, cur);
-
             if (pos < n)
-                ans = max(ans, cur + maxi[pos]);
+                ans = max(ans, post_sum[pos] + events[i][2]);
         }
 
         return ans;
